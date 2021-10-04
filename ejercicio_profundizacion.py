e891bd64d7946ea0a0f16bde8012123e7cf2be06
Julián Koroluk <julian.koroluk@outlook.com>
2021-10-04 20:51:26 -0300

    Create ejercicio_profundizacion.py
    Comienzo de ejercicio de profundización

---
+++ ejercicio_profundizacion.py
@@ -0,0 +1,23 @@
+import requests
+import json
+import matplotlib as plt
+
+def fetch():
+    pass
+
+
+def transform(dataset, min, max):
+    pass
+
+
+def report(data):
+    pass
+
+
+if __name__ == "_main_":
+    min = 0
+    max = 1
+
+    dataset = fetch()
+    data = transform(dataset, min, max)
+    report(data)
