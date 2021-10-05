6760cfb52503ddcbe345e117a3728feac2667472
Julián Koroluk <julian.koroluk@outlook.com>
2021-10-05 20:58:36 -0300

    Update ejercicio_profundizacion.py
    Ejercicio Profundización completo

--- ejercicio_profundizacion.py
+++ ejercicio_profundizacion.py
@@ -1,35 +1,91 @@
 import requests
 import json
-import matplotlib as plt
+import matplotlib.pyplot as plt
+import matplotlib.axes
 
-url = 'https://api.mercadolibre.com/sites/MLA/search?category=MLA1459&q=Departamentos%20Alquileres%20Mendoza%20&limit=50'
 
-def fetch():
+def fetch(ciudad):
     '''
-    Consumir datos
+    Consumo de datos
     
     Consume los datos de la 'url' y los filtra según su "__"currency_id"__".
     Retorna 'dataset' que es una lista de diccionarios.
-    dataset[] = {"price": ..., "condition": ...}
+
+    dataset = [{"price": ..., "condition": ...}, ...]
     '''
+    if ' ' in ciudad:
+        ciudad = ciudad.replace(' ', '%20')
+    
+    url = 'https://api.mercadolibre.com/sites/MLA/search?category=MLA1459&q=Departamentos%20Alquileres%20'+ciudad+'%20&limit=50'
     response = requests.get(url)
     data = response.json()
-    dataset = [] # Filtra el json
-    pass
+    dataset = [{'price': item["price"], 'condition': item["condition"]} for item in data["results"] if item["currency_id"] == "ARS"] # Filtra el json
+    return dataset
 
 
 def transform(dataset, min, max):
-    pass
+    '''
+    Transformación de la información
+
+    Filtra y divide el 'dataset' en tres listas distintas, separandolas según su 'price'.
+    Retorna 1 lista con el tamaño de cada una de las listas filtradas:
+    min_count: Cantidad de elementos con 'price' < 'min'
+    min_max_count: Cantidad de elementos con min < 'price' < 'max'
+    max_count: Cantidad de elementos con 'price' > 'max'
+
+    @param List dataset Diccionarios con el 'price' y 'condition' de c/ elemento
+    @param Float min Precio mínimo del rango
+    @param Float max Precio máximo del rango
+    '''
+    min_price = [x for x in dataset if x['price'] < min]
+    min_max_price = [x for x in dataset if min < x['price'] < max]
+    max_price = [x for x in dataset if x['price'] > max]
+    precios = [x['price'] for x in dataset]
+    #areas = [x['area'] for x in dataset]
+
+    min_count = len(min_price)
+    min_max_count = len(min_max_price)
+    max_count = len(max_price)
+
+    return [min_count, min_max_count, max_count]
 
 
-def report(data):
-    pass
+def report(data, min, max):
+    '''
+    Reporte
+    
+    Realiza un gráfico de torta que nos de la idea de como se reparten los precios 
+    de los alquileres según los parámetros "min" y "max".
+
+    @param List data Cantidades de elementos según los rangos de precios
+    @param Float min Precio mínimo del rango
+    @param Float max Precio máximo del rango
+    '''
+    fig = plt.figure()
+    fig.suptitle('Departamentos según precio', fontsize=16)
+    ax = fig.add_subplot()
+
+    etiquetas = ['< $'+str(min), '$'+str(min)+'-'+'$'+str(max), '> $'+str(max)]
+
+    ax.pie(data, labels=etiquetas, autopct='%1.1f%%', shadow=True, startangle=90)
+    ax.axis('equal')
+    plt.show()
+
+    '''fig2 = plt.figure()
+    fig2.suptitle('Precios vs Área', fontsize=16)
+    ax2 = fig.add_subplot()
+    ax2.plot(data[3], data[4], c='darkcyan')
+    ax2.set_facecolor('whitesmoke')
+    ax2.grid('solid')
+    plt.show()'''
 
 
-if __name__ == "_main_":
-    min = 0
-    max = 1
+if __name__ == '__main__':
+    print('¡Bienvenido al Buscador de Alquiler de Departamentos!\nPowered by MELI')
+    ciudad = input('Ingrese la ciudad que desea buscar: ')
+    min = float(input('Ingrese precio mínimo: $'))
+    max = float(input('Ingrese precio máximo: $'))
 
-    dataset = fetch()
+    dataset = fetch(ciudad)
     data = transform(dataset, min, max)
-    report(data)
+    report(data, min, max)
