7e49a27df81870bab137394739d179f845af35f0
Julián Koroluk <julian.koroluk@outlook.com>
2021-10-04 21:05:42 -0300

    Update ejercicio_profundizacion.py

--- ejercicio_profundizacion.py
+++ ejercicio_profundizacion.py
@@ -5,6 +5,16 @@ import matplotlib as plt
 url = 'https://api.mercadolibre.com/sites/MLA/search?category=MLA1459&q=Departamentos%20Alquileres%20Mendoza%20&limit=50'
 
 def fetch():
+    '''
+    Consumir datos
+    
+    Consume los datos de la 'url' y los filtra según su "__"currency_id"__".
+    Retorna 'dataset' que es una lista de diccionarios.
+    dataset[] = {"price": ..., "condition": ...}
+    '''
+    response = requests.get(url)
+    data = response.json()
+    dataset = [] # Filtra el json
     pass
 
 
